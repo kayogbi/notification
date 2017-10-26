4f270ab9372b3da4c99a63bf669a2e4d29ba18e5
Kayo <kayoalvestj.gbi@gmail.com>
2017-10-26 18:03:59 -0200

    v1.0.1

--- test/index.php
+++ test/index.php
@@ -3,5 +3,6 @@
 
 use notification\Email;
 
+
 $novoEmail = new Email(2,'mail.host.com','user@mail.com','pass','(tsl / ssl) ','port(587)','user@mail.com.br','userNome');
 $novoEmail->sendmail('Assunto','mensagem','Rem_email','Rem_nome','Des_email','Des_name');
